43082255cb2def564a12e0ae2d839251f5a9ba44
Jennings Anderson <jennings.anderson@colorado.edu>
2016-07-27 20:37:45 -0600

    adding the use

--- results-aggregator.js
+++ results-aggregator.js
@@ -162,3 +162,75 @@ var GenderHandler = function(){
           });
   }
 }
+
+
+
+var UseHandler = function(){
+  this.totals = {"use-no": 0,
+                 "use-yes-work": 0,
+                 "use-yes-personal": 0 }
+
+  this.options = Object.keys(this.totals);
+
+  this.count = function(tileProps){
+    for(var i=0; i<this.options.length; i++){
+      if(tileProps.hasOwnProperty(this.options[i])){
+        this.totals[this.options[i]] += tileProps[this.options[i]]
+      }
+    }
+  }
+
+  this.totalsArray = function(){
+    var arr = []
+    var that = this;
+    var prettyLabels = ['No', 'Work', 'Personal']
+    var ordered = ["use-no", "use-yes-work", "use-yes-personal"]
+    ordered.forEach(function(key,i){
+      arr.push({label: prettyLabels[i], value: that.totals[key]})
+    })
+    return arr;
+  }
+
+  this.buildPieChart = function(where){
+    data = this.totalsArray()
+    var w = 100,
+        h = 100,
+        r = 50
+
+    var vis = d3.select(where)
+        .append("svg:svg")              //create the SVG element inside the <body>
+        .data([data])                   //associate our data with the document
+            .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+            .attr("height", h)
+        .append("svg:g")                //make a group to hold our pie chart
+            .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
+
+    var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+        .outerRadius(r);
+
+    var pie = d3.layout.pie()           //this will create arc data for us given a list of values
+        .value(function(d) { return d.value; });    //we must tell it out to access the value of each element in our data array
+
+    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
+        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+            .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+                .attr("class", "slice");    //allow us to style things in the slices (like text)
+
+        arcs.append("svg:path")
+          .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
+          .attr("d", arc)                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+          .attr('style','opacity:0.75')
+          .on('mouseover',function(d){
+            d3.select(this).attr('style','opacity:1')
+            d3.select('#tooltip')
+              .style("display", "block")
+              .style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px")
+              .text(d.data.label + ": " + d.data.value)
+          })
+          .on('mouseout',function(e){
+            d3.select(this).attr('style','opacity:0.75')
+            d3.select('#tooltip').style('display','none')
+          });
+  }
+}
